97a3762447b3b57242a882c582c89ef228b8bc81
Administrator <Administrator@SC-201709231305>
2018-02-05 09:52:49 +0800

    wuwuwuu

---
+++ wujing/src/wujing/Wujing.java
@@ -0,0 +1,10 @@
+package wujing;
+
+public class Wujing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("eeeeeeeeeeeeeeeeeeeeee");
+	}
+
+}
